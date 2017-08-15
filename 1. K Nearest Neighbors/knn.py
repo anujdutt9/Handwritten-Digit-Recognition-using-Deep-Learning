4b0a6ad455d9914017c5613e5e171575fe02c393
Anuj Dutt <anuj.dutt9@gmail.com>
2017-08-15 15:08:30 -0400

    Update knn.py
    1. Log file creation code added
    2. Confusion Matrix code added
    3. Test Data Plotting code added

--- 1. K Nearest Neighbors/knn.py
+++ 1. K Nearest Neighbors/knn.py
@@ -1,18 +1,27 @@
-import matplotlib.pyplot as plt
+# K Nearest Neighbors
 
-from sklearn import cross_validation
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-from mnist import MNIST
-
+import sys
 import numpy as np
 import pickle
+from sklearn import model_selection
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+from MNIST_Dataset_Loader.mnist_loader import MNIST
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use('ggplot')
 
-#digits = datasets.load_digits()
-#print(digits.data)
+
+
+old_stdout = sys.stdout
+log_file = open("summary.log","w")
+sys.stdout = log_file
+
 
 print('\nLoading MNIST Data...')
-data = MNIST('./python-mnist/data/')
+# data = MNIST('./python-mnist/data/')
+
+data = MNIST('./MNIST_Dataset_Loader/python-mnist/data/')
 
 print('\nLoading Training Data...')
 img_train, labels_train = data.load_training()
@@ -31,8 +40,8 @@ X = train_img
 #Labels
 y = train_labels
 
-print('\nPreparing Classifier Training and Testing Data...')
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.1)
+print('\nPreparing Classifier Training and Validation Data...')
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X,y,test_size=0.1)
 
 
 print('\nKNN Classifier with n_neighbors = 5, algorithm = auto, n_jobs = 10')
@@ -47,17 +56,64 @@ pickle_in = open('MNIST_KNN.pickle','rb')
 clf = pickle.load(pickle_in)
 
 print('\nCalculating Accuracy of trained Classifier...')
-acc = clf.score(X_test,y_test)
+confidence = clf.score(X_test,y_test)
 
-print('\nMaking Predictions on Testing Data...')
-pred = clf.predict(test_img)
+print('\nMaking Predictions on Validation Data...')
+y_pred = clf.predict(X_test)
 
 print('\nCalculating Accuracy of Predictions...')
-accuracy = accuracy_score(test_labels, pred)
+accuracy = accuracy_score(y_test, y_pred)
 
-print('\nKNN Trained Classifier Accuracy: ',acc)
-print('\nPredicted Values: ',pred)
-print('\nAccuracy of Classifier on Test Images: ',accuracy)
+print('\nCreating Confusion Matrix...')
+conf_mat = confusion_matrix(y_test,y_pred)
 
+print('\nKNN Trained Classifier Confidence: ',confidence)
+print('\nPredicted Values: ',y_pred)
+print('\nAccuracy of Classifier on Validation Image Data: ',accuracy)
+print('\nConfusion Matrix: \n',conf_mat)
+
+
+# Plot Confusion Matrix Data as a Matrix
+plt.matshow(conf_mat)
+plt.title('Confusion Matrix for Validation Data')
+plt.colorbar()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
+
+
+print('\nMaking Predictions on Test Input Images...')
+test_labels_pred = clf.predict(test_img)
+
+print('\nCalculating Accuracy of Trained Classifier on Test Data... ')
+acc = accuracy_score(test_labels,test_labels_pred)
+
+print('\n Creating Confusion Matrix for Test Data...')
+conf_mat_test = confusion_matrix(test_labels,test_labels_pred)
+
+print('\nPredicted Labels for Test Images: ',test_labels_pred)
+print('\nAccuracy of Classifier on Test Images: ',acc)
+print('\nConfusion Matrix for Test Data: \n',conf_mat_test)
+
+# Plot Confusion Matrix for Test Data
+plt.matshow(conf_mat_test)
+plt.title('Confusion Matrix for Test Data')
+plt.colorbar()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
+
+sys.stdout = old_stdout
+log_file.close()
+
+
+
+# Show the Test Images with Original and Predicted Labels
+a = np.random.randint(1,50,20)
+for i in a:
+	two_d = (np.reshape(test_img[i], (28, 28)) * 255).astype(np.uint8)
+	plt.title('Original Label: {0}  Predicted Label: {1}'.format(test_labels[i],test_labels_pred[i]))
+	plt.imshow(two_d, interpolation='nearest',cmap='gray')
+	plt.show()
 #------------------------- EOC -----------------------------
 
