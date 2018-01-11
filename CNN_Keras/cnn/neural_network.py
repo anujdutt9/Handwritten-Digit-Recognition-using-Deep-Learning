9ba2418fdfc1a3cd34ad3b31fa913057be2ef89e
Anuj Dutt <anuj.dutt9@gmail.com>
2018-01-11 10:53:33 -0500

    Update
    Updated the Convolution2D to Conv2D
    Updated MaxPooling2D
    Theano Backend added for using input as (depth, height, width).
    To use Tensorflow as backend, use input as (height, width, depth)

--- CNN_Keras/cnn/neural_network.py
+++ CNN_Keras/cnn/neural_network.py
@@ -1,42 +1,46 @@
 from keras.models import Sequential
-from keras.layers.convolutional import Convolution2D
-from keras.layers.convolutional import MaxPooling2D
+from keras.layers.convolutional import Conv2D
+from keras.layers import MaxPooling2D
 from keras.layers.core import Activation
 from keras.layers.core import Flatten
 from keras.layers.core import Dense
 
+
+
 class CNN:
-	def build(Width, Height, Depth, total_classes, Saved_Weights_Path=None):
-		# Initialize the Model
-		model = Sequential()
+    @staticmethod
+    def build(width, height, depth, total_classes, Saved_Weights_Path=None):
+        # Initialize the Model
+        model = Sequential()
 
-		# First CONV => RELU => POOL Layer
-		model.add(Convolution2D(20, 5, 5, border_mode="same", input_shape=(Depth, Height, Width)))
-		model.add(Activation("relu"))
-		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        # First CONV => RELU => POOL Layer
+        model.add(Conv2D(20, 5, 5, border_mode="same", input_shape=(depth, height, width)))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), dim_ordering="th"))
 
-		# Second CONV => RELU => POOL Layer
-		model.add(Convolution2D(50, 5, 5, border_mode="same"))
-		model.add(Activation("relu"))
-		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-		
-		# Third CONV => RELU => POOL Layer 
-		# Convolution -> ReLU Activation Function -> Pooling Layer
-		model.add(Convolution2D(100, 5, 5, border_mode="same"))
-		model.add(Activation("relu"))
-		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        # Second CONV => RELU => POOL Layer
+        model.add(Conv2D(50, 5, 5, border_mode="same"))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), dim_ordering="th"))
 
-		# FC => RELU layers
-		# Fully Connected Layer -> ReLU Activation Function
-		model.add(Flatten())
-		model.add(Dense(500))
-		model.add(Activation("relu"))
+        # Third CONV => RELU => POOL Layer
+        # Convolution -> ReLU Activation Function -> Pooling Layer
+        model.add(Conv2D(100, 5, 5, border_mode="same"))
+        model.add(Activation("relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), dim_ordering="th"))
 
-		# Using Softmax Classifier for Linear Classification
-		model.add(Dense(total_classes))
-		model.add(Activation("softmax"))
+        # FC => RELU layers
+        #  Fully Connected Layer -> ReLU Activation Function
+        model.add(Flatten())
+        model.add(Dense(500))
+        model.add(Activation("relu"))
 
-		# If the saved_weights file is already present i.e model is pre-trained, load that weights
-		if weightsPath is not None:
-			model.load_weights(Saved_Weights_Path)
-		return model
+        # Using Softmax Classifier for Linear Classification
+        model.add(Dense(total_classes))
+        model.add(Activation("softmax"))
+
+        # If the saved_weights file is already present i.e model is pre-trained, load that weights
+        if Saved_Weights_Path is not None:
+            model.load_weights(Saved_Weights_Path)
+        return model
+# --------------------------------- EOC ------------------------------------
