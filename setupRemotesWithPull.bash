4265f3dd00c3dc7835f37f8c58b3d8c18d1dca0c
Banee Ishaque K <baneeishaque@hotmail.com>
2021-04-26 12:09:44 +0000

    Sync.

---
+++ setupRemotesWithPull.bash
@@ -0,0 +1,5 @@
+git pull upstream master
+git remote add github https://github.com/anujdutt9/Handwritten-Digit-Recognition-using-Deep-Learning.git
+git pull github master
+git remote add github-fork https://github.com/Baneeishaque/Handwritten-Digit-Recognition-using-Deep-Learning.git
+git pull github-fork master
